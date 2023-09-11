563616dd418b2cb44692c53a2ce783b5d2fb9244
raykr <jingzonglei@163.com>
2023-09-11 15:54:34 +0800

    nni configs

--- run_nni.sh
+++ run_nni.sh
@@ -1,2 +1,9 @@
 # ps -A | grep Main_Thread | awk '{print $1}' | xargs kill -9
-nnictl create --config ./nni/configs/nni-explore-mappo.yaml --port 8080
+# nnictl create --config ./nni/configs/nni-explore-mappo.yaml --port 8080
+# nnictl create --config ./nni/configs/nni-optimizer-gamma.yaml --port 8087
+# nnictl create --config ./nni/configs/nni-optimizer-lr.yaml --port 8088
+
+# traitor
+nnictl create --config ./nni/configs/traitor/nni-explore-mappo.yaml --port 8090
+nnictl create --config ./nni/configs/traitor/nni-optimizer-gamma.yaml --port 8091
+nnictl create --config ./nni/configs/traitor/nni-optimizer-lr.yaml --port 8092
