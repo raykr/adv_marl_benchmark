1baf823a5dbf40a59521df7b5a63ec26606f7881
raykr <jingzonglei@163.com>
2023-11-11 00:55:45 +0800

    update batch experiment scripts

--- scripts/exp.py
+++ scripts/exp.py
@@ -1,11 +1,9 @@
 import argparse
+import os
 import subprocess
+import time
 from concurrent.futures import ThreadPoolExecutor
 
-
-
-# 最大并发执行的命令数量
-max_concurrent_commands = 3
 
 # 定义一个函数来执行单个命令
 def execute_command(command):
@@ -15,18 +13,33 @@ def execute_command(command):
             return
         
         # 从 command 中提取 exp_name 后面的字符串 python -u ../single_train.py --env smac --env.map_name 3m --algo mappo --run single --exp_name mappo_smac_3m_gae_false --algo.use_gae False
+        env_name = command.split("--env")[1].split("--")[0].strip()
+        map_name = command.split("--env.map_name")[1].split("--")[0].strip()
+        algo_name = command.split("--algo ")[1].split("--")[0].strip()
         exp_name = command.split("--exp_name")[1].split("--")[0].strip()
-        with open(f'logs/{exp_name}_stdout.log', 'w') as stdout_file, open(f'logs/{exp_name}_stderr.log', 'w') as stderr_file:
+        trick_name = exp_name.replace(f"{algo_name}_{env_name}_{map_name}_", "")
+        folder = f"{env_name}/{map_name}/{algo_name}/{trick_name}"
+        os.makedirs(f"logs/{folder}", exist_ok=True)
+        
+        with open(f'logs/{folder}/stdout.log', 'w') as stdout_file, open(f'logs/{folder}/stderr.log', 'w') as stderr_file:
             print(f"Executing command: {command}")
             process = subprocess.Popen(
                 command, shell=True, stdout=stdout_file, stderr=stderr_file, text=True
             )
+            time.sleep(2)
             process.wait()  # 等待命令执行完成
 
         if process.returncode == 0:
             return f"Command '{command}' executed successfully"
         else:
-            return f"Error executing command '{command}', please check the logs\n"
+            error_output = stderr_file.read()
+            if "Error" in error_output:
+                print(f"Error detected in stderr for command '{command}'. Restarting the process...")
+                process.terminate() # 终止进程
+                time.sleep(2) 
+                return execute_command(command)
+            else:
+                return f"Error executing command '{command}', please check the logs\n"
     except Exception as e:
         return f"Error executing command '{command}': {str(e)}\n"
 
@@ -40,6 +53,13 @@ def main():
         required=True,
         help="You should provide a bash file with commands to execute.",
     )
+    parser.add_argument(
+        "-n",
+        "--num_workers",
+        default=4,
+        type=int,
+        help="Number of workers to use for parallel execution.",
+    )
     args, _ = parser.parse_known_args()
     args = vars(args)
     
@@ -48,7 +68,7 @@ def main():
         commands = file.readlines()
 
     # 使用ThreadPoolExecutor来控制并发执行
-    with ThreadPoolExecutor(max_concurrent_commands) as executor:
+    with ThreadPoolExecutor(args["num_workers"]) as executor:
         results = list(executor.map(execute_command, commands))
 
     # 打印执行结果
