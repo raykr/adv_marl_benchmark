bfb8e4f5b58a61774426a88880b8a15c71fbfa91
raykr <jingzonglei@163.com>
2024-01-07 11:12:26 +0800

    don't use gamma=1.0

--- experiment/generate.py
+++ experiment/generate.py
@@ -71,7 +71,7 @@ def generate_train_scripts(env, scenario, algo, out_dir, config_path=None, trick
                     _write_train_command(f, config_path, exp_name, logs_dir, trick_str=trick_str, trick=trick)
 
     print(f"You can run the following command to train all experiments:")
-    print(f"\n    python parallel -s {file_name} -n 2 \n")
+    print(f"\n    python parallel.py -s {file_name} -n 2 -o {out_dir} \n")
 
 
 def _write_train_command(file, config_path, exp_name, logs_dir, trick_str="", trick=None):
@@ -164,7 +164,7 @@ def generate_eval_scripts(env, scenario, algo, out_dir, slice=False, stage=0, tr
 
             
     print(f"You can run the following command to eval experiments:")
-    print(f"\n    python parallel -s {file_name} -n 2 -o {out_dir} \n")
+    print(f"\n    python parallel.py -s {file_name} -n 2 -o {out_dir} \n")
     if stage == 1:
         print(f"After all of the above experiments have been completed, you can generate stage 2 scripts with the following command: ")
         print(f"\n    python generate.py eval -e {env} -s {scenario} -a {algo} --stage 2   \n")
